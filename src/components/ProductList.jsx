481888dfb8703c83722a106d34eac7c6ce5c822e
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-05-31 15:01:20 +0200

    feat:add select category

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,16 @@
 import React,{useState,useEffect} from 'react'
+import {ProductCard} from './ProductCard';
+import styles from './ProductList.module.css'
 
 const ProductList=()=> {
     const [products, setProducts] = useState([])
+    const [selectedCategory, setselectedCategory] = useState([])
+
+const categories=[...new Set(products.map((product)=> product.category))];
+
     useEffect(()=>{
         fetch('https://dummyjson.com/products')
-        .then((response)=> response.json)
+        .then((response)=> response.json())
         .then((data)=>{
             setProducts(data.products);
             console.log(data.products)
@@ -12,10 +18,37 @@ const ProductList=()=> {
         .catch((error)=>{
             console.log('error fetching products', error)
         });
-    },
-   [] )
+    },[] )
+    const handleCategorySelect=(event)=>{
+        setselectedCategory(event.target.value);
+    }
   return (
-    <div>ProductList</div>
+    <div className={styles.listWrapper}>
+        <div className={styles.controlsWrapper}>
+        <label htmlFor='category'>Filter by category:</label>
+        <select
+          id='category'
+          className={styles.inputField}
+          value={selectedCategory}
+          onChange={handleCategorySelect}
+        >
+          <option value=''>All</option>
+          { categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+           ))
+          }
+        </select>
+      </div>
+      <div className={styles.cardsWrapper}>
+        {products
+        .filter((product)=>selectedCategory==''|| product.category===selectedCategory)
+        .map((product)=>(
+            <ProductCard key={product.id} product={product}/>
+        ))}
+      </div>
+    </div>
   );
 }
 
